514c087764624bef63df581f98009dc25ae44262
Belazor <9424286+filliph@users.noreply.github.com>
2023-05-14 19:38:08 +0100

    Update deDE.lua

--- locales/deDE.lua
+++ locales/deDE.lua
@@ -4,14 +4,26 @@ if not L then return end
 
 L["%s Upgrades"] = "%s Aufwertungen"
 L["(Weekly cap reached)"] = "(Wöchentliches Limit erreicht)"
+--[[Translation missing --]]
+L["Aspect's Crests"] = true
 L["Cost for next level:"] = "Kosten für die nächste Stufe:"
 L["Cost to upgrade to max level:"] = "Kosten um auf die maximal Stufe aufzuwerten:"
 L["Currency needed for max level:"] = "Für die maximal Stufe benötigte Währung:"
 L["Currency remaining after upgrading:"] = "Verbleibende Währung nach dem Aufwerten:"
+--[[Translation missing --]]
+L["Drake's Crests"] = true
 L["Epic callings left: %s"] = "Epische Berufungen übrig: %s"
+--[[Translation missing --]]
+L["Flightstone / Crest Upgrades"] = true
+--[[Translation missing --]]
+L["Flightstones"] = true
 L["Heirloom Upgrades"] = "Erbstückaufwertungen"
 L["Item can be upgraded to max level!"] = "Gegenstand kann auf die maximal Stufe aufgewertet werden!"
 L["Item upgraded to max level!"] = "Gegenstand auf maximal Stufe aufgewertet!"
 L["M+ runs left: %s"] = "Noch benötigte M+ Läufe: %s"
 L["Rare callings left: %s"] = "Seltene Berufungen übrig: %s"
 L["Upgrade level: %d / %d"] = "Aufwertungsstufe: %d / %d"
+--[[Translation missing --]]
+L["Whelpling's Crests"] = true
+--[[Translation missing --]]
+L["Wyrm's Crests"] = true
