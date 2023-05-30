be8930ef3b252f4f1077384bf5d49fe4ed19e76c
Belazor <9424286+filliph@users.noreply.github.com>
2023-05-30 17:27:44 +0100

    Add zhTW translation (thanks BNS333 @ CF)

--- locales/zhTW.lua
+++ locales/zhTW.lua
@@ -11,4 +11,26 @@ if not L then
     return
 end
 
-
+L["(Weekly cap reached)"] = "(已達到每週上限)"
+L["M+ runs left: %s"] = "傳奇+次數還需: %s"
+L["Rare callings left: %s"] = "稀有的召喚還需: %s"
+L["Epic callings left: %s"] = "史詩的召喚還需: %s"
+L["Heirloom Upgrades"] = "傳家寶升級"
+L["%s Upgrades"] = "%s 升級"
+L["Item upgraded to max level!"] = "物品已升到最大等級！"
+L["Item can be upgraded to max level!"] = "物品可以升級到滿等！"
+L["Upgrade level: %d / %d"] = "提升等級: %d / %d"
+L["Cost for next level:"] = "下個等級花費:"
+L["Cost to upgrade to max level:"] = "升到滿等花費:"
+L["Currency needed for max level:"] = "到滿等所需通貨:"
+L["Currency remaining after upgrading:"] = "升級後剩餘通貨:"
+L["Flightstone / Crest Upgrades"] = "龍石 / 紋章升級"
+L["Whelpling's Crests"] = "幼龍紋章"
+L["Drake's Crests"] = "飛龍紋章"
+L["Wyrm's Crests"] = "巨龍紋章"
+L["Aspect's Crests"] = "守護巨龍紋章"
+L["Whelpling's Crest Fragments"] = "幼龍紋章碎片"
+L["Drake's Crest Fragments"] = "飛龍紋章碎片"
+L["Wyrm's Crest Fragments"] = "巨龍紋章碎片"
+L["Aspect's Crest Fragments"] = "守護巨龍紋章碎片"
+L["Flightstones"] = "龍石"
