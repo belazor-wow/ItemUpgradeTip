a26aba0ce374846589103c983912c1aef5933b1f
Justin Driggers <justin@justindriggers.com>
2024-05-02 13:00:51 -0400

    Season 4 update

--- mixins/tabs/upgrade/UpgradeMixin.lua
+++ mixins/tabs/upgrade/UpgradeMixin.lua
@@ -46,6 +46,13 @@ local UPGRADE_DATA_PROVIDER_LAYOUT = {
     },
     {
         headerTemplate = "ItemUpgradeTipStringColumnHeaderTemplate",
+        headerText = _G["ITEMUPGRADETIP_L_UPGRADE_TRACK_AWAKENED"],
+        headerParameters = { "awakenedTier" },
+        cellTemplate = "ItemUpgradeTipStringCellTemplate",
+        cellParameters = { "awakenedTier" },
+    },
+    {
+        headerTemplate = "ItemUpgradeTipStringColumnHeaderTemplate",
         headerText = _G["ITEMUPGRADETIP_L_CREST_TYPE"],
         headerParameters = { "crestType" },
         cellTemplate = "ItemUpgradeTipStringCellTemplate",
@@ -68,23 +75,38 @@ function ItemUpgradeTipUpgradeDataProviderMixin:Refresh()
 
         local rank1 = upgradeTrack.upgrade1.rank
         local rank2 = nil
+        local rank3 = nil
+        local rank4 = nil
 
         local upgradeTierString1 = UPGRADE_TIER_FORMAT_STRING:format(upgradeTrack.upgrade1.upgradeLevel, upgradeTrack.upgrade1.maxUpgradeLevel)
         local upgradeTierString2 = nil
+        local upgradeTierString3 = nil
+        local upgradeTierString4 = nil
 
         if upgradeTrack.upgrade2 ~= nil then
             rank2 = upgradeTrack.upgrade2.rank
             upgradeTierString2 = UPGRADE_TIER_FORMAT_STRING:format(upgradeTrack.upgrade2.upgradeLevel, upgradeTrack.upgrade2.maxUpgradeLevel)
         end
 
+        if upgradeTrack.upgrade3 ~= nil then
+            rank3 = upgradeTrack.upgrade3.rank
+            upgradeTierString3 = UPGRADE_TIER_FORMAT_STRING:format(upgradeTrack.upgrade3.upgradeLevel, upgradeTrack.upgrade3.maxUpgradeLevel)
+        end
+
+        if upgradeTrack.upgrade4 ~= nil then
+            rank4 = upgradeTrack.upgrade4.rank
+            upgradeTierString4 = UPGRADE_TIER_FORMAT_STRING:format(upgradeTrack.upgrade4.upgradeLevel, upgradeTrack.upgrade4.maxUpgradeLevel)
+        end
+
         local upgradeInfo = {
             ilvl = upgradeTrack.itemLevel,
             crestType = icon .. " " .. upgradeTrack.currency.color:WrapTextInColorCode(upgradeTrack.currency.name),
-            adventurerTier = (rank1 == 2 and upgradeTierString1) or (rank2 == 2 and upgradeTierString2) or "",
-            veteranTier = (rank1 == 3 and upgradeTierString1) or (rank2 == 3 and upgradeTierString2) or "",
-            championTier = (rank1 == 4 and upgradeTierString1) or (rank2 == 4 and upgradeTierString2) or "",
-            heroTier = (rank1 == 5 and upgradeTierString1) or (rank2 == 5 and upgradeTierString2) or "",
-            mythTier = (rank1 == 6 and upgradeTierString1) or (rank2 == 6 and upgradeTierString2) or "",
+            adventurerTier = (rank1 == 2 and upgradeTierString1) or (rank2 == 2 and upgradeTierString2) or (rank3 == 2 and upgradeTierString3) or (rank4 == 2 and upgradeTierString4) or "",
+            veteranTier = (rank1 == 3 and upgradeTierString1) or (rank2 == 3 and upgradeTierString2) or (rank3 == 3 and upgradeTierString3) or (rank4 == 3 and upgradeTierString4) or "",
+            championTier = (rank1 == 4 and upgradeTierString1) or (rank2 == 4 and upgradeTierString2) or (rank3 == 4 and upgradeTierString3) or (rank4 == 4 and upgradeTierString4) or "",
+            heroTier = (rank1 == 5 and upgradeTierString1) or (rank2 == 5 and upgradeTierString2) or (rank3 == 5 and upgradeTierString3) or (rank4 == 5 and upgradeTierString4) or "",
+            mythTier = (rank1 == 6 and upgradeTierString1) or (rank2 == 6 and upgradeTierString2) or (rank3 == 6 and upgradeTierString3) or (rank4 == 6 and upgradeTierString4) or "",
+            awakenedTier = ((rank1 == 7 or rank1 == 8) and upgradeTierString1) or ((rank2 == 7 or rank2 == 8) and upgradeTierString2) or ((rank3 == 7 or rank3 == 8) and upgradeTierString3) or ((rank4 == 7 or rank4 == 8) and upgradeTierString4) or "",
             index = index,
             selected = self:IsSelected(index),
             crestTypeCurrencyId = upgradeTrack.currency.currencyId
